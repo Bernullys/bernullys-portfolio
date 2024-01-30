c60902ebe112a4701d4dfb3a7e1394cb404e56cb
badr <bernardoantoniod@gmail.com>
2024-01-30 16:56:44 -0300

    a little bit of styles

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -4,20 +4,27 @@ function ProjectCard () {
     return (
         <section className="project_main_container">
             <article className="title_description_container">
-                <h1>Dog's Lover</h1>
+                <h3>Dog's Lover</h3>
                 <p>This is build to enjoy doggys</p>
             </article>
             <article className="tools_used_container">
                 <h4>Made with</h4>
                 <section className="tool_container">
                     <h5>HTML</h5>
+                    <h5>CSS</h5>
                     <h5>JavaScript</h5>
                 </section>
             </article>
             <article className="view_project_container">
                 <section className="view_buttons_container">
-                    <button>View online</button>
-                    <button>Repo on GitHub</button>
+                    <ul>
+                        <li>
+                            <a href="">View online</a>
+                        </li>
+                        <li>
+                            <a href="">Repo on GitHub</a>
+                        </li>
+                    </ul>
                 </section>
             </article>
         </section>
