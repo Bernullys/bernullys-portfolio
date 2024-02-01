ab9c6c9995284fec011c1df28754df9cb75da020
badr <bernardoantoniod@gmail.com>
2024-01-31 22:19:00 -0300

    ProjectCards mapping and conditional rendering

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,28 +1,33 @@
 import "./ProjectCard.css"
 
-function ProjectCard () {
+function ProjectCard ({ title, description, tools, onlineLink, repo } ) {
     return (
         <section className="project_main_container">
             <article className="title_description_container">
-                <h3>Dog's Lover</h3>
-                <p>This is build to enjoy doggys</p>
+                <h3>{ title }</h3>
+                <p>{ description }</p>
             </article>
             <article className="tools_used_container">
                 <h4>Made with</h4>
                 <section className="tool_container">
-                    <h5>HTML</h5>
-                    <h5>CSS</h5>
-                    <h5>JavaScript</h5>
+                    {/* This is easier to understand, but is the long way: */}
+                    {/* {tools.html && <h5>{tools.html}</h5>}*/}
+                    {/* This is better if I do it being focus */}
+                    {
+                        Object.entries(tools).map(([key, value]) => (
+                            value && <h5 key={key}>{value}</h5>
+                        ))
+                    }
                 </section>
             </article>
             <article className="view_project_container">
                 <section className="view_buttons_container">
                     <ul>
                         <li>
-                            <a href="">View online</a>
+                            <a href={ onlineLink }>View online</a>
                         </li>
                         <li>
-                            <a href="">Repo on GitHub</a>
+                            <a href={ repo }>Repo on GitHub</a>
                         </li>
                     </ul>
                 </section>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,6 +2,29 @@ import Layout from "./Layout"
 import ProjectCard from "./ProjectCard"
 import "./Projects.css"
 
+const myProjects = [
+    {
+        title: "Dog's Lovers",
+        description: "An app for dog lovers. Enjoy random adorable dog pictures. Save your favorites. Upload your own dog photos.",
+        tools: {
+            html: "HTML",
+            css: "CSS",
+            javaScript: "JavaScript",
+        },
+        onlineLink: "",
+        repo: ""
+    },
+    {
+        title: "ToDo App",
+        description: "This is a straightforward to-do app, designed for practicing DOM manipulation. It utilizes a single HTML file for simplicity.",
+        tools: {
+            html: "HTML",
+        },
+        onlineLink: "",
+        repo: ""
+    },
+]
+
 function Projects () {
     return (
         <Layout>
@@ -9,8 +32,19 @@ function Projects () {
                 <article>
                     <h2>My Projects</h2>
                 </article>
-                <section>
-                    <ProjectCard/>
+                <section className="projects_cards_container">
+                    {
+                        myProjects.map(project => (
+                            <ProjectCard
+                                key={project.title}
+                                title={project.title}
+                                description={project.description}
+                                tools={project.tools}
+                                onlineLink={project.onlineLink}
+                                repo={project.repo}  
+                            />
+                        ))
+                    }
                 </section>
             </section>
         </Layout>
